c1c05d4f3931585310a4cf4a04c85fa2930e612e
Maxwell Evans <maxwellthefourth@gmail.com>
2023-10-16 21:57:49 -0600

    added Surf Dismount Ground Effects and Shop Items by Badge Count

--- src/field_player_avatar.c
+++ src/field_player_avatar.c
@@ -1685,6 +1685,7 @@ static void Task_WaitStopSurfing(u8 taskId)
         gPlayerAvatar.preventStep = FALSE;
         UnlockPlayerFieldControls();
         DestroySprite(&gSprites[playerObjEvent->fieldEffectSpriteId]);
+        playerObjEvent->triggerGroundEffectsOnMove = TRUE;
         DestroyTask(taskId);
     }
 }
